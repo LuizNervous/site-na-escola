3e218e2b023543fdd519d525fe490a237123e0d2
LuizNervous <luiz.gu.neves@gmail.com>
2025-06-09 09:44:25 -0300

    mudanças

---
+++ script.js
@@ -0,0 +1,13 @@
+ lucide.createIcons();
+
+    const topBtn = document.getElementById("topBtn");
+    window.onscroll = () => {
+      topBtn.style.display = window.scrollY > 200 ? "block" : "none";
+    };
+    topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+    const toggleTheme = document.getElementById("toggleTheme");
+    toggleTheme.onclick = () => {
+      document.body.classList.toggle("dark-mode");
+      toggleTheme.textContent = document.body.classList.contains("dark-mode") ? "☀️ Modo Claro" : "🌙 Modo Escuro";
+    };
